f947c876029af9a858a1202d3eb70351508c0530
ronejunior88 <rone.junior88@gmail.com>
2023-06-02 11:20:30 -0300

    Ajustes

--- BackEnd/src/FinSys/FinSys.Service/Expendings/AddExpendingService/AddExpendingService.cs
+++ BackEnd/src/FinSys/FinSys.Service/Expendings/AddExpendingService/AddExpendingService.cs
@@ -1,14 +1,26 @@
 ﻿using FinSys.Service.Domain;
 using FinSys.Service.Interfaces;
 using MediatR;
+using Microsoft.Extensions.Configuration;
 
 namespace FinSys.Service.Expendings.AddExpendingService
 {
     public class AddExpendingService : IAddExpendingService
     {
+        private readonly IConfiguration _configuration;
+        public AddExpendingService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public Task<Unit> AddExpending(ExpendingDTO expending)
         {
-            throw new NotImplementedException();
+
+        }
+
+        public string GetConnection()
+        {
+            var connection = _configuration.GetSection("ConnectionStrings").GetSection("ProdutoConnection").Value;
+            return connection;
         }
     }
 }
--- BackEnd/src/FinSys/FinSys/Program.cs
+++ BackEnd/src/FinSys/FinSys/Program.cs
@@ -1,3 +1,6 @@
+
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Get connection Strings
+var finSysConnection = builder.Configuration.GetConnectionString("FinSys");
+
 
 var app = builder.Build();
 
