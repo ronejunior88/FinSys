fb0a4d7de6cc44c0184620df09b616a426434e31
ronejunior88 <rone.junior88@gmail.com>
2023-10-18 21:06:36 -0300

    Ajuste Service SQL Parameters

--- BackEnd/src/FinSys/FinSys.Service/Expendings/AddExpendingService/AddExpendingService.cs
+++ BackEnd/src/FinSys/FinSys.Service/Expendings/AddExpendingService/AddExpendingService.cs
@@ -29,8 +29,23 @@ namespace FinSys.Service.Expendings.AddExpendingService
 
             using (SqlConnection connection = new SqlConnection(_connection))
             {
-                var command = $"INSERT INTO Expending ([Id], [Value], [Description]) VALUES ('{expending.Id}', '{expending.Value}', '{expending.Description}')";
-                await connection.ExecuteAsync(command);
+                connection.Open();
+
+                string sqlQuery = "INSERT INTO Expending ([Id], [Value], [Description]) VALUES (@Id, @Value, @Description)";
+
+                using (SqlCommand cmd = new SqlCommand(sqlQuery,connection))
+                {
+                    cmd.Parameters.Add("@Id",SqlDbType.UniqueIdentifier);
+                    cmd.Parameters["@Id"].Value = expending.Id;
+
+                    cmd.Parameters.Add("@Value", SqlDbType.Real);
+                    cmd.Parameters["@Value"].Value = expending.Value;
+
+                    cmd.Parameters.Add("@Description", SqlDbType.NVarChar, 100);
+                    cmd.Parameters["@Description"].Value = expending.Description;
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                }
             }            
         }
     }
