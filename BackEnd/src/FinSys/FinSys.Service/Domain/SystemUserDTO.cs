9e9b9149f97fb62f1674227a0236dd04a0d0e2ed
ronejunior88 <rone.junior88@gmail.com>
2024-03-22 10:24:22 -0300

    Adicionando commands do SystemUser

---
+++ BackEnd/src/FinSys/FinSys.Service/Domain/SystemUserDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinSys.Service.Domain
+{
+    public class SystemUserDTO
+    {
+        public SystemUserDTO()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateBirth { get; set; }
+    }
+}
---
+++ BackEnd/src/FinSys/FinSys.Service/SystemUser/AddSystemUserService/AddSystemUserService.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using FinSys.Service.Domain;
+using FinSys.Service.Interfaces;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+namespace FinSys.Service.SystemUser.AddSystemUserService
+{
+    public class AddSystemUserService : IAddSystemUserService
+    {
+        private readonly IConfiguration _configuration;
+        private string _connection;
+
+        public AddSystemUserService()
+        { }
+
+        public AddSystemUserService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task AddSystemUser(SystemUserDTO user)
+        {
+            _connection = _configuration.GetConnectionString("FinSys");
+
+            using (SqlConnection connection = new SqlConnection(_connection))
+            {
+                connection.Open();
+
+                string sqlQuery = @"INSERT INTO SystemUser ([Id], [Name], [DateBirth]) VALUES (@Id, @Name, @DateBirth)";
+
+                await connection.ExecuteAsync(sqlQuery, user);
+
+                connection.Close();
+            }
+        }
+    }
+}
